15f40fe09c2c26638208cf5494d5ed14863c5725
Dibyajyoti Kumar <dibyacodes@gmail.com>
2024-11-10 07:31:38 +0530

    feat : Added task edit functionality

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,87 +1,81 @@
+
 const inputField = document.querySelector("#input")
 const submit = document.querySelector("#submit")
 const mainTaskDiv = document.querySelector("#task")
-const editButton = document.querySelector("#edit")
 displayIfNotNull()
+const ParentEditButton = document.querySelector("#edit")
+
 
 // generate a random task ID for every Task
-function randomTaskId(){
-    let randomTaskItemId = Math.floor(Math.random()*10000)
+function randomTaskId() {
+    let randomTaskItemId = Math.floor(Math.random() * 10000)
     return randomTaskItemId
 }
 
 
 // display the list on page load if not null
-function displayIfNotNull(){
+function displayIfNotNull() {
     let userTaskListOnLoad = JSON.parse(localStorage.getItem("userDetailsArray")) || []
-    if (userTaskListOnLoad.length > 0){
+    if (userTaskListOnLoad.length > 0) {
         displayUserTasks()
     }
 }
 
 // Checks if the userInput is an empty string or not
-function checkIfAnInvalidString(string){
-    if (string.length === 0){
-        return true 
+function checkIfAnInvalidString(string) {
+    if (string.length === 0) {
+        return true
     } else {
         return false
     }
 }
 
 // Create a new input with a checkbox
-function createUserTask(userTaskLabel,divId){
+function createUserTask(userTaskLabel, divId) {
     const inputDiv = document.createElement("div")
-    inputDiv.setAttribute("class","userInputDiv")
-    inputDiv.setAttribute("id",divId)
-    
+    inputDiv.setAttribute("class", "userInputDiv")
+    inputDiv.setAttribute("id", divId)
+
     const divForEditAndRemoveButton = document.createElement("div")
-    divForEditAndRemoveButton.setAttribute("class","editAndRemoveButton")
-    
-    
+    divForEditAndRemoveButton.setAttribute("class", "editAndRemoveButton")
+
     // Defining the edit button
     const editButton = document.createElement("button")
-    editButton.setAttribute("id","edit")
+    editButton.setAttribute("class", "edit")
     editButton.innerHTML = `Edit`
-    
+
     // Defining the remove button
     const removeButton = document.createElement("button")
-    removeButton.setAttribute("id","remove")
+    removeButton.setAttribute("class", "remove")
     removeButton.innerHTML = `Remove`
-    
-    
+
+
     const divForInputAndLabel = document.createElement("div")
-    divForInputAndLabel.setAttribute("class","divForInputAndLabel")
+    divForInputAndLabel.setAttribute("class", "divForInputAndLabel")
     const inputElement = document.createElement("input")
-    
-    inputElement.setAttribute("class","usertask")
-    inputElement.setAttribute("type","checkbox")
-    inputElement.setAttribute("name","userTaskInput")
+
+    inputElement.setAttribute("class", "usertask")
+    inputElement.setAttribute("type", "checkbox")
+    inputElement.setAttribute("name", "userTaskInput")
     const labelElement = document.createElement("label")
-    
+
     labelElement.textContent = `${userTaskLabel}`
-    labelElement.setAttribute("for","task")
-    labelElement.setAttribute("class","taskLabel")
-    
+    labelElement.setAttribute("for", "task")
+    labelElement.setAttribute("class", "taskLabel")
+
     // Defining what will the edit and remove buttons do
-    
+
     // Remove Button
-    removeButton.addEventListener('click',()=>{
-        console.log(`Clicked On ${removeButton.innerHTML}`);
+    removeButton.addEventListener('click',() => {
         removeUserFromLocalStorage(divId)
         displayIfNotNull()
     })
-    
+
     // Edit Button
-    editButton.addEventListener('click',()=>{
-        console.log(`Clicked On ${editButton.innerHTML}`);
+    editButton.addEventListener('click', () => {
+        editUserTask(divId)
     })
-    
-    divForInputAndLabel.addEventListener('click',(event)=>{
-        console.log(event.target)
-        // delete event.target
-        inputElement.checked = true
-    })
-    
+
     divForInputAndLabel.appendChild(inputElement)
     divForInputAndLabel.appendChild(labelElement)
     divForEditAndRemoveButton.appendChild(removeButton)
@@ -92,72 +86,93 @@ function createUserTask(userTaskLabel,divId){
 }
 
 // accept inputs in an object and stores it as string in local storage
-function storeObjectsAsStringLocally(uId,userTaskString){
+function storeObjectsAsStringLocally(uId, userTaskString) {
     // Gets All the Data in an Object
     let dataObject = {
-        userId : uId, // stored as a number ID
-        task : `${userTaskString}`,
-        completedStatus : false
+        userId: uId, // stored as a number ID
+        task: `${userTaskString}`,
+        completedStatus: false
     }
-    
+
     // Checks existing values from the web localstorage
     let storeArray = JSON.parse(localStorage.getItem("userDetailsArray")) || [] // Unpacking the array because it is stored as a string
-    
+
     storeArray.push(dataObject)
-    
+
     // Conver the JSON object into a string
     let stringObject = JSON.stringify(storeArray) // packing the array
-    
-    localStorage.setItem("userDetailsArray",`${stringObject}`)
+
+    localStorage.setItem("userDetailsArray", `${stringObject}`)
 }
 
-function displayUserTasks(){
+function displayUserTasks() {
     mainTaskDiv.innerHTML = ''
-    
+
     // Get data from Local Storage of the browser
     const getLocalStorage = JSON.parse(localStorage.getItem("userDetailsArray")) || []
-    
+
     getLocalStorage.forEach(element => {
-        createUserTask(element.task,element.userId)
+        createUserTask(element.task, element.userId)
     });
 }
 
 // Lets the user remove tasks on completion. Should remove it completely from the local storage
-function removeUserFromLocalStorage(userElement){
+function removeUserFromLocalStorage(userElement) {
     const getItemFromLocalstorage = JSON.parse(localStorage.getItem("userDetailsArray")) || []
     getItemFromLocalstorage.forEach(element => {
-        if (userElement === element.userId){
-            console.log(element);
+        if (userElement === element.userId) {
             let indexElement = getItemFromLocalstorage.indexOf(element)
-            console.log(indexElement);
-            getItemFromLocalstorage.splice(indexElement,1)
-            
+            getItemFromLocalstorage.splice(indexElement, 1)
+
             let modifiedArray = JSON.stringify(getItemFromLocalstorage)
-            localStorage.setItem("userDetailsArray",`${modifiedArray}`)
+            localStorage.setItem("userDetailsArray", `${modifiedArray}`)
         }
-        if (getItemFromLocalstorage.length == 0){
+
+        if (getItemFromLocalstorage.length === 0) {
             displayIfNotNull()
             localStorage.clear()
         }
     });
-    
-    
 }
 
-submit.addEventListener('click',()=>{
+
+// Edit task and then store it to the local storage.
+function editUserTask(targetID) {
+    let UserLocalStorage = JSON.parse(localStorage.getItem("userDetailsArray"))
+    let taskElement
+
+    UserLocalStorage.forEach((element) => {
+        if (targetID == element.userId) {
+            taskElement = element
+        }
+    })
+
+    inputField.value = taskElement.task
+    submit.style.display = "none"
+    ParentEditButton.style.display = "block"
+
+    ParentEditButton.addEventListener('click', () => {
+        if (taskElement.task !== inputField.value) {
+            taskElement.task = inputField.value
+            let modifiedList = JSON.stringify(UserLocalStorage)
+            localStorage.setItem("userDetailsArray", modifiedList)
+            displayIfNotNull()
+            submit.style.display = "block"
+            ParentEditButton.style.display = 'none'   
+        }   
+    })
+}
+
+submit.addEventListener('click', () => {
     const userTask = inputField.value
     const validOrInvalid = checkIfAnInvalidString(userTask)
-    
-    if (validOrInvalid === false){
+
+    if (validOrInvalid === false) {
         let randomId = randomTaskId()
-        new storeObjectsAsStringLocally(randomId,userTask)
+        new storeObjectsAsStringLocally(randomId, userTask)
     }
     displayUserTasks()
     inputField.value = ''
 })
 
 displayIfNotNull()
-
-
-
-
